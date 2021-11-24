412c1bada3b578de3fb36f5f03e9ef0ff762634d
Vitaly Posadnev <elemabor@gmail.com>
2021-11-24 19:41:42 +0300

    feat(urls): include urls for lab2 to main project

--- tau/urls.py
+++ tau/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 import lab1_pvv
+import lab2_pvv
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('lab1_pvv/', include('lab1_pvv.urls')),
+    path('lab2_pvv/', include('lab2_pvv.urls')),
 ]
