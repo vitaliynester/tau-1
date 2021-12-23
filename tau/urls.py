de1ec30914d716038646e776fb33c83dd632e711
Vitaly Posadnev <elemabor@gmail.com>
2021-12-23 19:11:58 +0300

    feat(url): add link to lab3

--- tau/urls.py
+++ tau/urls.py
@@ -1,26 +1,13 @@
-"""tau URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 
 import lab1_pvv
 import lab2_pvv
+import lab3_pvv
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('lab1_pvv/', include('lab1_pvv.urls')),
     path('lab2_pvv/', include('lab2_pvv.urls')),
+    path('lab3_pvv/', include('lab3_pvv.urls')),
 ]
